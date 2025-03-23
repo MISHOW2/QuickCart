1acfd6526fae1f08b95fc763bc2d14bab7ec66e3
MISHOW2 <mshibambo63@gmail.com>
2025-03-23 08:22:14 +0200

    push

--- server/server.js
+++ server/server.js
@@ -8,16 +8,17 @@ app.use(express.json());
 // Middleware to serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+const productRoutes = require("./routes/productRoutes");
+app.use("/api", productRoutes);
 
-// Use the routes properly
-app.use('/auth', require('./routes/authRoutes')); 
-app.use('/api' , require('./routes/productRoutes'))
+
 // Middleware to serve static files
 
 
 const products = require("./config/db");
 
 
-
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
